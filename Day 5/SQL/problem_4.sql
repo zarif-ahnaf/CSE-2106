8fce5c7cebfa8188b3ca5e7582273a320ea6b295
zarif-ahnaf <86199070+zarif-ahnaf@users.noreply.github.com>
2024-08-24 01:18:01 +0600

    Fix files
    f

---
+++ Day 5/SQL/problem_4.sql
@@ -0,0 +1,3 @@
+SELECT book_id, member_id 
+FROM BorrowedBook 
+WHERE borrowed_date BETWEEN '2024-01-01' AND '2024-08-20';
--- Day 5/problem_4.sql
+++
@@ -1,3 +0,0 @@
-SELECT book_id, member_id 
-FROM BorrowedBook 
-WHERE borrow_date BETWEEN '2024-01-01' AND '2024-08-20';
